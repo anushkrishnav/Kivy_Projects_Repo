04710601ca988b02b729a092995213a4bd075ddc
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-05-11 19:20:32 +0530

    Removed unrelated files

--- Real_time_Api.py
+++
@@ -1,9 +0,0 @@
-import requests
-url = 'http://api.airvisual.com/v2/nearest_city?key=f7316a56-f7eb-4c55-9b8e-af9877f75d6a'
-payload = {}
-files = {}
-headers= {}
-
-response = requests.request("GET", url, headers=headers, data = payload, files = files)
-
-print(response.text.encode('utf8'))
